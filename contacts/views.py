fce32a1fcd82d42181c3c7646544e1b92db8c34a
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-04-14 17:39:54 +0530

    CSV download

--- contacts/views.py
+++ contacts/views.py
@@ -5,11 +5,24 @@ from contacts.models import *
 from django.views.generic import UpdateView, CreateView, ListView
 from accounts.models import Account
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+import csv
 
-
+@csrf_exempt
 def homeView(request):
-    return render(request, 'contacts/home.html')
+	if request.method == 'POST':
+		user = request.POST.get('user')
+		response = HttpResponse(content_type='text/csv')
+		response['Content-Disposition'] = 'attachment; filename="Contact.csv"'
+		writer = csv.writer(response)
+		writer.writerow(['Name', 'Phone Number', 'Description'])
+		instance = Contact.objects.all()
+		for contact in instance:
+			if str(contact.user_name) == str(user):
+				writer.writerow([contact.name, contact.contact_number, contact.description])
+		return response
+	return render(request, 'contacts/home.html')
 
 @login_required
 def listContactsView(request):
